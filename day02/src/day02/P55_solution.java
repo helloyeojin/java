2fa7a9ea5a9a4665a7d8c8b32ba1d4eb0db40702
helloyeojin <bonuibomb@gmail.com>
2023-02-23 09:36:06 +0900

    2023-02-23 P55_solution

---
+++ day02/src/day02/P55_solution.java
@@ -0,0 +1,13 @@
+package day02;
+
+public class P55_solution {
+
+	public static void main(String[] args) {
+		long a = 2000000000;
+		long b = 2000000000;
+		
+		long result = a + b;
+		System.out.println(result);
+	}
+
+}
