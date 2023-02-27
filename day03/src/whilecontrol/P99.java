799b39e5dddbc568a21633dae40c9edba0a9cd2e
helloyeojin <bonuibomb@gmail.com>
2023-02-27 09:46:21 +0900

    test contents deleted

--- day03/src/whilecontrol/P99.java
+++ day03/src/whilecontrol/P99.java
@@ -2,22 +2,12 @@ package whilecontrol;
 
 public class P99 {
 
-   public static void main(String[] args) {
-	   
-     		 int aa = 100;
-    	int bb = 100;
-	
-	   int b = 20;
-	   int cc = 10;
-	  int c = 10;
-	  
-	  
-      for(int i=2;i<10;i++) {
-         System.out.printf("%d 단 \n",i);
-         for(int j=1;j<10;j++) {
-            System.out.printf("%d * %d = %d \n",i,j,(i*j));
+public static void main(String[] args) {
+	for(int i=2;i<10;i++) {
+		System.out.printf("%d 단 \n",i);
+		for(int j=1;j<10;j++) {
+			System.out.printf("%d * %d = %d \n",i,j,(i*j));
          }
       }
    }
-
 }
