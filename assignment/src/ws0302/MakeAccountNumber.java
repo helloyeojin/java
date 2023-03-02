6c4ddafca2554c0549e528cd52a2c9337ac39080
helloyeojin <bonuibomb@gmail.com>
2023-03-02 17:14:52 +0900

    2023-03-02 MakeAccountNumber 수

--- assignment/src/ws0302/MakeAccountNumber.java
+++ assignment/src/ws0302/MakeAccountNumber.java
@@ -1,16 +1,20 @@
 package ws0302;
 
 import java.util.Calendar;
+import java.util.Random;
 
 public class MakeAccountNumber {
 	
 	public static String makeAccNum() {
 		String accNum = "";
+		Random r = new Random();
+	
 		Calendar cal = Calendar.getInstance();
 		accNum = "" + cal.get(Calendar.YEAR) 
 					+ (cal.get(Calendar.MONTH)+1) 
 					+ cal.get(Calendar.DAY_OF_MONTH) 
-					+ cal.get(Calendar.MILLISECOND);
+					+ cal.get(Calendar.MILLISECOND)
+		            + r.nextInt(9);
 		return accNum;
 	}
 }
